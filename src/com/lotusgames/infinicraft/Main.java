4cc6ca61ca759a79054e5ee117a134f1202d793b
LotusBro98 <LotusBro98@yandex.ru>
2018-12-13 22:39:56 +0300

    Init with plugin

---
+++ src/com/lotusgames/infinicraft/Main.java
@@ -0,0 +1,15 @@
+package com.lotusgames.infinicraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
